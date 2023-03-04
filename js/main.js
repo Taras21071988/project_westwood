30fde57f1beed62db82d483516757551e38bf726
Taras21071988 <tsasojlov@icloud.com>
2023-03-04 20:15:07 +0200

    add swiper

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  grabCursor: true,
+  slidesPerView: 4,
+  spaceBetween: 17,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+
+  mousewheel: {
+    sensiviti: 1,
+    eventsTarget: ".swiper"
+  }
+
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+});
